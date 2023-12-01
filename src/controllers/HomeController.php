918649ba9a0840120bcf8551460e9458c1835d5d
sdlima4025 <sergiolimafg3@gmail.com>
2023-12-01 19:09:56 -0300

    criado rota de verificação de login

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -5,16 +5,13 @@ use \core\Controller;
 
 class HomeController extends Controller {
 
+    private $loggedUser;
+
+    public function __construct() {
+        $this->redirect('/login');
+    }
+
     public function index() {
         $this->render('home', ['nome' => 'Sérgio!']);
     }
-
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
-    }
-
 }
